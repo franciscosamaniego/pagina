f2ebd21faf98e66b0dda6e956e1010d5cb8d14e6
franciscosamaniego <franciscosamaniego.fs@gmail.com>
2022-05-05 12:30:16 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function convertir(e) {
+  //console.log(e.target);
+  //console.log(e.target.value);
+  let num = parseInt(e.target.value)
+  console.log(num);
+  console.log(typeof num);
+  let spans = document.querySelectorAll('span');
+  for(let s of spans) {
+    console.log(s.dataset.base);
+    s.textContent = num.toString(parseInt(s.dataset.base)).toUpperCase();
+  }
+}
